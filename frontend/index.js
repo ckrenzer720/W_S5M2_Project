f92ad91afb2c01b774fbf1cdcaf7feb13338fe39
Conner Krenzer <krenzer84@gmail.com>
2024-07-21 13:06:28 -0400

    almost there

--- frontend/index.js
+++ frontend/index.js
@@ -2,88 +2,129 @@
 // 👉 TASK 1 - Understand the existing code 👈
 function moduleProject2() {
   // 👇 WORK WORK BELOW THIS LINE 👇
-  let startTime = new Date().getTime() // Record start time
+  let startTime = new Date().getTime(); // Record start time
 
-  function getTimeElapsed() { // To be used at end of game to get elapsed time
-    let currentTime = new Date().getTime()
-    return currentTime - startTime
+  function getTimeElapsed() {
+    // To be used at end of game to get elapsed time
+    let currentTime = new Date().getTime();
+    return currentTime - startTime;
   }
 
   // Setting up the footer content
-  let footer = document.querySelector('footer')
-  let currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+  let footer = document.querySelector("footer");
+  let currentYear = new Date().getFullYear();
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`;
 
-  let keys = { // To easily check `event.key` on keyboard events
-    space: ' ',
-    up: 'ArrowUp',
-    right: 'ArrowRight',
-    down: 'ArrowDown',
-    left: 'ArrowLeft',
-  }
+  let keys = {
+    // To easily check `event.key` on keyboard events
+    space: " ",
+    up: "ArrowUp",
+    right: "ArrowRight",
+    down: "ArrowDown",
+    left: "ArrowLeft",
+  };
 
   // Helper function to grab all squares
-  const getAllSquares = () => document.querySelectorAll('.square')
+  const getAllSquares = () => document.querySelectorAll(".square");
 
   // Populating the grid with rows and squares
   for (let n = 0; n < 5; n++) {
     // Creating the rows
-    let row = document.createElement('div')
-    document.querySelector('#grid').appendChild(row)
-    row.classList.add('row')
+    let row = document.createElement("div");
+    document.querySelector("#grid").appendChild(row);
+    row.classList.add("row");
     // Creating the squares
     for (let m = 0; m < 5; m++) {
-      let square = document.createElement('div')
-      square.classList.add('square')
-      row.appendChild(square)
-      square.addEventListener('click', () => {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      row.appendChild(square);
+      square.addEventListener("click", () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
         // 👉 TASK 2 - Use a click handler to target a square 👈
-        if (!square.classList.contains('targeted')) {
-          document.querySelector('.targeted').classList.remove('targeted');
-          square.classList.add('targeted');
+        if (!square.classList.contains("targeted")) {
+          document.querySelector(".targeted").classList.remove("targeted");
+          square.classList.add("targeted");
         }
-      })
+      });
     }
   }
-  document.querySelector('.row:nth-child(3)')
-    .children[2].classList.add('targeted') // Initial square being targeted
+  document
+    .querySelector(".row:nth-child(3)")
+    .children[2].classList.add("targeted"); // Initial square being targeted
 
   // Helper function to obtain 5 random indices (0-24) to put mosquitoes in
   function generateRandomIntegers() {
-    let randomInts = []
+    let randomInts = [];
     while (randomInts.length < 5) {
-      let randomInt = Math.floor(Math.random() * 25)
+      let randomInt = Math.floor(Math.random() * 25);
       if (!randomInts.includes(randomInt)) {
-        randomInts.push(randomInt)
+        randomInts.push(randomInt);
       }
     }
-    return randomInts
+    return randomInts;
   }
-  let allSquares = getAllSquares()
-  generateRandomIntegers().forEach(randomInt => { // Puts live mosquitoes in 5 random squares
-    let mosquito = document.createElement('img')
-    mosquito.src = './mosquito.png'
-    mosquito.style.transform = `rotate(${Math.floor(Math.random() * 359)}deg) scale(${Math.random() * 0.4 + 0.8})`
-    mosquito.dataset.status = 'alive'
-    allSquares[randomInt].appendChild(mosquito)
-  })
+  let allSquares = getAllSquares();
+  generateRandomIntegers().forEach((randomInt) => {
+    // Puts live mosquitoes in 5 random squares
+    let mosquito = document.createElement("img");
+    mosquito.src = "./mosquito.png";
+    mosquito.style.transform = `rotate(${Math.floor(
+      Math.random() * 359
+    )}deg) scale(${Math.random() * 0.4 + 0.8})`;
+    mosquito.dataset.status = "alive";
+    allSquares[randomInt].appendChild(mosquito);
+  });
 
-  document.addEventListener('keydown', evt => {
+  document.addEventListener("keydown", (evt) => {
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
     // 👉 TASK 3 - Use the arrow keys to highlight a new square 👈
+    console.log(evt.key);
+    let arrowDown = evt.key === keys.down;
+    let arrowUp = evt.key === keys.up;
+    let arrowLeft = evt.key === keys.left;
+    let arrowRight = evt.key === keys.right;
+
+    let targeted = document.querySelector('.targeted')
+
+    if (arrowDown) {
+      if (targeted.parentElement.nextElementSibling) {
+        let index = Array.from(targeted.parentElement.children).indexOf(targeted);
+        targeted.classList.remove('targeted')
+        targeted.parentElement.nextElementSibling.children[index].classList.add('targeted')
+      }
+    } else if (arrowUp) {
+      if (targeted.parentElement.previousElementSibling) {
+        let index = Array.from(targeted.parentElement.children).indexOf(targeted);
+        targeted.classList.remove('targeted')
+        targeted.parentElement.previousElementSibling.children[index].classList.add('targeted')
+      }
+    } else if (arrowLeft) {
+      // console.log('you clicked left!');
+      if (targeted.previousElementSibling) {
+       targeted.classList.remove('targeted')
+       targeted.previousElementSibling.classList.add('targeted')
+      }
+    } else if (arrowRight) {
+      if (targeted.nextElementSibling) {
+        targeted.classList.remove('targeted')
+        targeted.nextElementSibling.classList.add('targeted')
+       }
+      // console.log('you clicked right!');
+    }
+
 
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
     // 👉 TASK 4 - Use the space bar to exterminate a mosquito 👈
 
     // 👉 TASK 5 - End the game 👈
     // 👉 TASK 5 - End the game 👈
-  })
+  });
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
 // ❗ DO NOT MODIFY THE CODE BELOW
 // ❗ DO NOT MODIFY THE CODE BELOW
 // ❗ DO NOT MODIFY THE CODE BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = { moduleProject2 }
-else moduleProject2()
+if (typeof module !== "undefined" && module.exports)
+  module.exports = { moduleProject2 };
+else moduleProject2();
